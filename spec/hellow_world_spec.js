f00d3b4224e346b13f655df6cc073ee9ecfc0894
Iran Corrales Becerra <iranhoe@live.com.mx>
2018-04-21 11:58:46 -0500

    fix unit test

--- spec/hellow_world_spec.js
+++ spec/hellow_world_spec.js
@@ -3,7 +3,7 @@ var main = require("../Scripts/main");
 describe("Hello World Test", function() {
     describe("GET /", function() {
         it("returns status code 200", function () {
-            main.
+            
         });
     });
 });
