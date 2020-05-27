a062e4d55e6f6d04ccd260684784ffd527fb86b6
Iran Corrales Becerra <iranhoe@live.com.mx>
2020-05-26 18:22:39 -0700

    remove style and javascript

--- spec/hellow_world_spec.js
+++
@@ -1,9 +0,0 @@
-var main = require("../Scripts/main");
-
-describe("Hello World Test", function() {
-    describe("GET /", function() {
-        it("returns status code 200", function () {
-            
-        });
-    });
-});
